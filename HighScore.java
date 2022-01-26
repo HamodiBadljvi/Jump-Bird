acab952c461e9a099e77dd9c6b58049bdc0472ce
Adde0k <andkar0729@skola.goteborg.se>
2022-01-26 12:16:03 +0100

    First Commit. Highscore.

---
+++ HighScore.java
@@ -0,0 +1,18 @@
+public class HighScore {
+    private int highScore;
+
+    public int getHighScore() {
+        return highScore;
+    }
+
+    public void setHighScore(int score) {
+        if (score > highScore) {
+            this.highScore = score;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "HighScore: " + highScore;
+    }
+}
