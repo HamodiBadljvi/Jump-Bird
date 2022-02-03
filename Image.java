9f2b5c540329a0d7b8f477e2b4c93c7e0b8c0fc0
HamodiBadljvi <mohbad0315@skola.goteborg.se>
2022-02-03 08:54:14 +0100

    Bättre formaterat

--- Image.java
+++ Image.java
@@ -6,24 +6,23 @@ public class Image {
     static JFrame f;
     static JLabel l;
 
-
     ImageIcon background;
 
-    public void background(){
-    try{
-        background = new ImageIcon(getClass().getResource("background"));
-        //display image on screen
-        l = new JLabel(background);
-        f = new JFrame();
-        f.add(l);
-    } catch(Exception e){
-        System.out.println("Image not found!");
-    }
-    //set size of background image(change x)
-    //f.setSize(x, x);
-    f.pack();
-    f.setVisible(true);
+    public void background() {
+        try {
+            // get image
+            background = new ImageIcon(getClass().getResource("background"));
+            // display image on screen?
+            l = new JLabel(background);
+            f = new JFrame();
+            f.add(l);
+        } catch (Exception e) {
+            System.out.println("Image not found!");
+        }
+        // set size of background image(change x)
+        // f.setSize(x, x);
+        f.pack();
+        f.setVisible(true);
 
     }
 }
-
