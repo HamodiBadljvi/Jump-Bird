2c1258c9d16a11184e93b96182ec2b3993fcc399
AntRanYRGO <antran0410@skola.goteborg.se>
2022-02-11 09:47:05 +0100

    Changing PlayableCharacter to use Rect2d & point2d

--- PlayableCharacter.java
+++ PlayableCharacter.java
@@ -1,43 +1,41 @@
-import java.util.Arrays;
+import java.awt.geom.Point2D;
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
 
 public class PlayableCharacter {
-    // position[0] is x (horisontal) and position[1] is y (vertical)
-    //Maybe x axis is of no use
-    private int[] position = new int[2];
-    private int[] size = new int[2];
 
-    public PlayableCharacter() {
-        this.position[0] = 10;
-        this.position[1] = 10;
-        this.size[0] = 10;
-        this.size[1] = 10;
+    private Point2D position2d;
+    private Rectangle2D rect2d;
 
+    //Creates character with rectangle that has position (0,0) and size (0,0)
+    public PlayableCharacter(){
+        this.rect2d = new Rectangle2D.Double();
     }
 
-    public PlayableCharacter(int posx, int posy, int height, int width) {
-        this.position[0] = posx;
-        this.position[1] = posy;
-        this.size[0] = height;
-        this.size[1] = width;
-
+    //Creates character that is a rectangle with width and height
+    //that has its top left corner in point2D
+    public PlayableCharacter(double width, double height, Point2D point2D){
+        this.position2d = new Point2D.Double(point2D.getX(), point2D.getY());
+        this.rect2d = new Rectangle2D.Double(point2D.getX(), point2D.getY(),width,height);
+    }
+    public void setPosition2d(Point2D p){
+        this.position2d = p;
     }
 
-    public int[] getPosition() {
-        return position;
+    public Point2D getPosition2d() {
+        return position2d;
     }
 
-    public void setPosition(int[] position) {
-        this.position = position;
+    public Rectangle2D getRect2d() {
+        return rect2d;
     }
 
     @Override
     public String toString() {
-        return "PlayableCharacter [position x =" + position[0] + " position y " + position[1]
-                + ", size=" + size[0] + " x " + size[1] + "]";
-    }
-
-    public void yMovement() {
-        // TODO
-        // Method for moving character on y-axis
+        return "Character{" +
+                "rect2d=" + rect2d +
+                ", position2d=" + position2d +
+                '}';
     }
 }
+
