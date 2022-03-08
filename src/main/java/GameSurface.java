579c0f33801bf47205c2e368bae818be2ba594ca
Eleni Solomon-Samuelsson <elesol2001@skola.goteborg.se>
2022-03-08 11:55:31 +0100

    Uppdating new versions of the character

--- src/main/java/GameSurface.java
+++ src/main/java/GameSurface.java
@@ -39,7 +39,7 @@ public class GameSurface extends JPanel implements KeyListener, MouseListener, A
 
     public GameSurface(final int width, final int height) {
         try {
-            this.monkeySprite = ImageIO.read(new File("src/main/resources/senaste apan.png"));
+            this.monkeySprite = ImageIO.read(new File("src/main/resources/senaste_apan.png"));
             this.background = ImageIO.read(new File("src/main/resources/background.png"));
         } catch (Exception e) {
             System.err.println("Monke problem");
